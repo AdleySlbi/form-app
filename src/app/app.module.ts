d4797606c4e4dc5280cbd9117ba0aaab3ae91980
AdSlb <adley.salabi@gmail.com>
2022-06-27 16:17:34 +0200

    feat(main-form) : created the first route of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainFormComponent } from './main-form/main-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFormComponent
   ],
   imports: [
     BrowserModule,
