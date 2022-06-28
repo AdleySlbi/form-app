f9758a759f6eeaa1dae08e176552176c86ef63c7
AdSlb <adley.salabi@gmail.com>
2022-06-28 17:03:31 +0200

    feat(quizz) : preparing the workflow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modules 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Components
 import { AppComponent } from './app.component';
 import { MainFormComponent } from './main-form/main-form.component';
+
+
 
 @NgModule({
   declarations: [
