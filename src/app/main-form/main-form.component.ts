4922106cc83e08a4e0eba68e39475f07f67ad649
AdSlb <adley.salabi@gmail.com>
2022-07-08 13:11:41 +0200

    feat(yearPicker)

--- src/app/main-form/main-form.component.ts
+++ src/app/main-form/main-form.component.ts
@@ -6,7 +6,8 @@ import { MatStepper, StepperOrientation } from '@angular/material/stepper';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-// import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { MatDatepicker } from '@angular/material/datepicker';
 
 export const MY_FORMATS = {
   parse: {
@@ -19,19 +20,23 @@ export const MY_FORMATS = {
   },
 };
 
+
 @Component({
   selector: 'app-main-form',
   templateUrl: './main-form.component.html',
   styleUrls: ['./main-form.component.scss'],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' },
-    // {
-    //   provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
-    // },
-    // { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+    {
+      provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
   ]
 })
 export class MainFormComponent implements OnInit {
+
+  @ViewChild('picker', { static: false })
+  private picker!: MatDatepicker<Date>;  
 
   stepperOrientation: Observable<StepperOrientation>;
 
@@ -94,9 +99,9 @@ export class MainFormComponent implements OnInit {
   chosenYearHandler(ev:any, input:any) {
     let { _d } = ev;
     this.personnalInformation.value.birthYear = _d;
-    input._destroyPopup()
+    // input._destroyPopup()
+    this.picker.close()
   }
-
 
   finalSendToApi() {
     if (this.personnalInformation.value.livingType == "house" && this.wekeep == true) {
