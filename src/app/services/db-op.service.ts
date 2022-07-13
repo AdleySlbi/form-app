83b293f57a6e2e7a6b5a5548602ed74780f0aa70
AdSlb <adley.salabi@gmail.com>
2022-07-13 18:49:03 +0200

    feat(cors) : updated headers

--- src/app/services/db-op.service.ts
+++ src/app/services/db-op.service.ts
@@ -9,20 +9,35 @@ export class DbOpService {
 
   constructor(private http: HttpClient) { }
 
-  getWeKeep(dept: number): Observable<any>{
+  getWeKeep(dept: number): Observable<any> {
     console.log(dept)
     return this.http.get(`https://script.google.com/macros/s/AKfycbzo1GjM407_blAni_qYBuEFFhaWa3LevHk9jJ1M_Qkp9vlMZ6iWV7dMKcHSMJXDnH1e/exec?sq_dept=${dept}&sq_offer=PV`)
   }
 
-  postApi1(data: any): Observable<any>{
-    return this.http.post(`https://script.google.com/macros/s/AKfycbzUoZGKsPk-crUwcMRniz-UnqbfJ9T5fMWUpW2Dl7F6W0ilDXAsAWpDCdG4daf5DxQguA/exec`, data)
-  }
-  
-  postCid(cid: String): Observable<any>{
-    return this.http.post(`https://limsorts-oscularly.icu/postback?cid=${cid}&payout=27`, null)
+  postApi1(data: any): Observable<any> {
+    return this.http.post(`https://script.google.com/macros/s/AKfycbzUoZGKsPk-crUwcMRniz-UnqbfJ9T5fMWUpW2Dl7F6W0ilDXAsAWpDCdG4daf5DxQguA/exec`, data, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+    )
   }
 
-  postApi2(data: any): Observable<any>{
-    return this.http.post(`https://ios14club.herokuapp.com/offers/1/webhook`, data)
+  postCid(cid: String): Observable<any> {
+    return this.http.post(`https://limsorts-oscularly.icu/postback?cid=${cid}&payout=27`, null, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
   }
+
+  postApi2(data: any): Observable<any> {
+    return this.http.post(`https://ios14club.herokuapp.com/offers/1/webhook`, data, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+  }
+
+
 }
